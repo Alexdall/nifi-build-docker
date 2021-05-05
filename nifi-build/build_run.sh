7084fad11b1d5c89d10464c7ec81a24d093676e3
Alexandre <alexandredall@yahoo.com.br>
2021-05-05 00:06:36 -0300

    Add files via upload

--- nifi-build/build_run.sh
+++ nifi-build/build_run.sh
@@ -3,12 +3,13 @@ PORT=$2
 set -x
 mkdir -p ./output
 chmod a+rw ./output
-docker build -t nifi-$dname/alexdall .
+docker build -t nifi-$dname/alexandre .
 docker run --rm --name nifi-$dname \
   -p $PORT:$PORT \
   -v $(pwd)/output:/opt/host-output \
   -d \
+  --log-opt max-size=30m --log-opt max-file=10 \
   -e NIFI_WEB_HTTP_PORT=$PORT \
-  nifi-$dname/alexdall:latest
+  nifi-$dname/alexandre:latest
 
 
